32c4de795898ca96b92181facbea20ce5bb688bd
Saif Rahman <Saf.saif@gmail.com>
2024-03-28 00:17:20 +0600

    first commit

---
+++ src/DataForPage/dummyData.jsx
@@ -0,0 +1,226 @@
+import chair from "../assets/images/Living Room.jpg";
+import sofa2 from "../assets/images/LivingRoom2.jpg";
+import sofa3 from "../assets/images/LivingRoom3.jpg";
+import phone1 from "../assets/images/bedRoom.png";
+import blueSofa from "../assets/images/trace.svg";
+
+export const navbarData = [
+  {
+    id: "1",
+    name: "News",
+    link: "news",
+  },
+  {
+    id: "2",
+    name: "Company",
+    link: "company",
+  },
+  {
+    id: "3",
+    name: "Features",
+    link: "features",
+  },
+  {
+    id: "4",
+    name: "Room",
+    link: "room",
+  },
+  {
+    id: "5",
+    name: "Demo",
+    link: "demo",
+  },
+  // {
+  //   id: "6",
+  //   name: "Login",
+  //   link: "/",
+  // },
+];
+export const FooterData1 = [
+  {
+    id: "1",
+    text: "Modern",
+  },
+  {
+    id: "2",
+    text: "Decorations",
+  },
+  {
+    id: "3",
+    text: "Events",
+  },
+  {
+    id: "4",
+    text: "Careere",
+  },
+  {
+    id: "5",
+    text: "Forum",
+  },
+  {
+    id: "6",
+    text: "Contacts",
+  },
+];
+export const FooterData2 = [
+  {
+    id: "1",
+    text: "Privacy policy",
+    path: "Resources",
+  },
+  {
+    id: "2",
+    text: "Terms of use",
+  },
+  {
+    id: "3",
+    text: "Cookies",
+  },
+];
+export const FooterData3 = [
+  {
+    id: "1",
+    text: "Support",
+  },
+  {
+    id: "2",
+    text: "Customer support",
+  },
+  {
+    id: "3",
+    text: "Media",
+  },
+  {
+    id: "4",
+    text: "Marketing",
+  },
+];
+
+export const sliderData = [
+  {
+    id: "1",
+    title: "Room 1",
+    text: "With Linear Architect's, your room will shine bright again. Let furniture find there place in new beautifule warm room.",
+    type: "Room1",
+    img: sofa2,
+  },
+  {
+    id: "2",
+    title: "Room 2",
+    text: "Linear Architect's is your best firend for interior design, download the app and feel free to explore beautifule modern and premium designs.",
+    type: "Room2",
+    img: chair,
+  },
+  {
+    id: "3",
+    title: "Room 3",
+    text: "Linear Architect's is your best firend for interior design, download the app and feel free to explore beautifule modern and premium designs.",
+    type: "Room3",
+    img: sofa3,
+  },
+];
+export const tabsData = [
+  {
+    id: "0",
+    label: "Bedroom",
+    value: "bedroom",
+    desc: `Linear Architect's is your best firend for interior design, download the app and feel free to explore beautifule modern and premium designs.`,
+    img: phone1,
+    
+  },
+  {
+    id: "1",
+    label: "LivingRoom",
+    value: "livingroom",
+    desc: "Linear Architect's is your best firend for interior design, download the app and feel free to explore beautifule modern and premium designs.",
+    img: phone1,
+    
+  },
+
+  {
+    id: "2",
+    label: "Kitchen",
+    value: "kitchen",
+    desc: `Linear Architect's is your best firend for interior design, download the app and feel free to explore beautifule modern and premium designs.`,
+    img: phone1,
+    
+  },
+
+  {
+    id: "3",
+    label: "Office",
+    value: "office",
+    desc: ` Linear Architect's is your best firend for interior design, download the app and feel free to explore beautifule modern and premium designs.`,
+    img: phone1,
+    
+  },
+
+  {
+    id: "4",
+    label: "KidsRoom",
+    value: "kidsroom",
+    desc: `Linear Architect's is your best firend for interior design, download the app and feel free to explore beautifule modern and premium designs.`,
+    img: phone1,
+    
+  },
+];
+export const cardData = [
+  {
+    id: "0",
+    title: "Ready to get started?",
+    text: "Explore Linear Architect's Pricing and chose the plan that suites you the best, or send as an inquery about invidual plan. You can also request a demo app to test in a secure enviroment.",
+  },
+  {
+    id: "1",
+    title: "Room",
+    text: "Customize your own Bedroom, LivingRoom, Kitchen, Office and much more. Anytime, in a matter of seconds.",
+  },
+  {
+    id: "2",
+    title: "Connect to AxeL",
+    text: "Setup your room on AxeL in a few seconds. All you need is few images and creative ideas and your business is ready.",
+  },
+];
+
+export const room = [
+  {
+    id: "1",
+    name: "Interior Design and Consultancy ",
+    img: blueSofa,
+  },
+  {
+    id: "2",
+    name: "Architectural Plan",
+    img: blueSofa,
+  },
+  {
+    id: "3",
+    name: "Structural Design",
+    img: blueSofa,
+  },
+  {
+    id: "4",
+    name: "Soil Test",
+    img: blueSofa,
+  },
+  {
+    id: "5",
+    name: "landscape Design",
+    img: blueSofa,
+  },
+  {
+    id: "6",
+    name: "Estimating",
+    img: blueSofa,
+  },
+  {
+    id: "7",
+    name: "Supervision",
+    img: blueSofa,
+  },
+  {
+    id: "8",
+    name: "Digital Survey",
+    img: blueSofa,
+  },
+];
