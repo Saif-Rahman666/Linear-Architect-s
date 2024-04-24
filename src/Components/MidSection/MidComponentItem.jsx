b5d0b1801eb9d83e4d425dc5652d17bc785e706e
Saif Rahman <Saf.saif@gmail.com>
2024-04-24 12:24:37 +0600

    our services background changed to glass

--- src/Components/MidSection/MidComponentItem.jsx
+++ src/Components/MidSection/MidComponentItem.jsx
@@ -66,12 +66,7 @@ const MidComponentItem = ({ title, img, text, reverse }) => {
           className="sm:flex-col flex flex-row-reverse justify-around items-center w-full"
         >
           <div className="sm:w-full sm:pl-4 pb-4 relative">
-            <div className="relative">
-              <div
-                className={`absolute inset-0 ${
-                  theme === "light" ? "bg-white" : "bg-black"
-                } opacity-50`}
-              ></div>
+            <div className={`bg-opacity-50 ${theme === "light" ? "bg-white" : "bg-black"} sm:bg-white rounded-lg p-4`}>
               <h2
                 className={
                   theme === "light"
@@ -81,10 +76,8 @@ const MidComponentItem = ({ title, img, text, reverse }) => {
               >
                 {title}
               </h2>
-            </div>
-            <div className="flex flex-col">
-              {room.map((item) => {
-                return (
+              <div className="flex flex-col">
+                {room.map((item) => (
                   <div className="flex items-center py-2" key={item.id}>
                     <div>
                       <img
@@ -93,31 +86,28 @@ const MidComponentItem = ({ title, img, text, reverse }) => {
                         alt="blueSofa"
                       ></img>
                     </div>
-                    <div
-                      className={`absolute inset-0 ${
-                        theme === "light" ? "bg-white" : "bg-black"
-                      } opacity-50`}
-                    ></div>
-                    <motion.div
-                      className={
-                        theme === "light"
-                          ? "sm:text-xl text-2xl font-normal font-inter no-underline align-middle tracking-wide normal-case leading-none text-dark w-full relative z-10"
-                          : "sm:text-xl text-2xl font-normal font-inter no-underline align-middle tracking-wide normal-case leading-none text-white w-full relative z-10"
-                      }
-                      initial="hidden"
-                      whileInView="visible"
-                      viewport={{ once: true }}
-                      transition={{ duration: 1.5 }}
-                      variants={{
-                        visible: { opacity: 1, scale: 1 },
-                        hidden: { opacity: 0, scale: 0 },
-                      }}
-                    >
-                      {item.name}
-                    </motion.div>
+                    <div className={`bg-opacity-50 ${theme === "light" ? "bg-white" : "bg-black"} sm:bg-white rounded-lg p-2`}>
+                      <motion.div
+                        className={
+                          theme === "light"
+                            ? "sm:text-xl text-2xl font-normal font-inter no-underline align-middle tracking-wide normal-case leading-none text-dark w-full relative z-10"
+                            : "sm:text-xl text-2xl font-normal font-inter no-underline align-middle tracking-wide normal-case leading-none text-white w-full relative z-10"
+                        }
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true }}
+                        transition={{ duration: 1.5 }}
+                        variants={{
+                          visible: { opacity: 1, scale: 1 },
+                          hidden: { opacity: 0, scale: 0 },
+                        }}
+                      >
+                        {item.name}
+                      </motion.div>
+                    </div>
                   </div>
-                );
-              })}
+                ))}
+              </div>
             </div>
           </div>
           <div>
