177ca76565817c7562d5c4e38ce4ee26b3548f9e
Saif Rahman <Saf.saif@gmail.com>
2024-04-25 12:56:48 +0600

    Newsletter width decreased

--- src/Components/MidSection/NewsLetter.jsx
+++ src/Components/MidSection/NewsLetter.jsx
@@ -4,7 +4,7 @@ const NewsLetter = () => {
 
   return (
     <div className="flex items-center justify-center content-center justify-items-center relative">
-      <div className="sm:flex-col sm:py-6 bottom-10 h-full flex justify-evenly bg-darkBlue w-3/4 py-10 rounded-xl translate-y-16 duration-700">
+      <div className="sm:flex-col sm:py-6 bottom-10 h-full flex justify-evenly bg-darkBlue w-2/4 py-10 rounded-xl translate-y-16 duration-700">
         <div>
           <h2 className="sm:text-lg text-2xl pt-2 pb-2 font-bold font-inter no-underline align-middle tracking-wide normal-case leading-none text-white pl-4">
             Send us an email at lineararchitectsltd@gmail.com
